7064abe70bfbf023754b8503aaa6856fe89a5bc6
JakeLeigh <jake@jakeleigh.io>
2019-03-31 13:59:15 +0100

    test data return with simple p tag

--- main.js
+++ main.js
@@ -1,69 +1,39 @@
-var pageCounter = 1;
 var moduleContainer = document.getElementById("module-info");
-var btn = document.getElementById("btn");
+let undergradBtn = document.querySelector("#undergrad");
+let postgradBtn = document.querySelector("#postgrad");
+let researchBtn = document.querySelector("#research");
 
-btn.addEventListener("click", function() {
-  var ourRequest = new XMLHttpRequest();
-  ourRequest.open(
-    "GET",
-    "https://raw.githubusercontent.com/JakeLeigh/CW2/master/module-" +
-      pageCounter +
-      ".json"
-  );
-  ourRequest.onload = function() {
-    //console.log(ourRequest.responseText);
-    var ourData = JSON.parse(ourRequest.responseText);
-    //console.log(ourData[0]);
-    renderHTML(ourData);
-  };
-  ourRequest.send();
-  pageCounter++;
-  if (pageCounter > 3) {
-    //btn.classList.add("hide-me");
-    btn.disabled = true;
-  }
+undergradBtn.addEventListener("click", function() {
+  axios
+    .get("https://raw.githubusercontent.com/JakeLeigh/CW2/master/module-1.json")
+    .then(response => {
+      let undergradData = response.data;
+      renderHTML(undergradData);
+    })
+    .catch(err => console.log(err));
 });
 
-function renderHTML(data) {
+// undergradBtn.addEventListener("click", function() {
+//   var ourRequest = new XMLHttpRequest();
+//   ourRequest.open(
+//     "GET",
+//     "https://raw.githubusercontent.com/JakeLeigh/CW2/master/module-1.json"
+//   );
+//   ourRequest.onload = function() {
+//     //console.log(ourRequest.responseText);
+//     var ourData = JSON.parse(ourRequest);
+//     renderHTML(ourData);
+//     console.log(ourData);
+//   };
+// });
+
+function renderHTML(courses) {
   var htmlString = "";
 
-  for (i = 0; i < data.length; i++) {
-    htmlString +=
-      "<p>" + data[i].Name + " is a " + data[i].Course + " has assements "; //".</p>";
-    for (ii = 0; ii < data[i].Module.Assignment.length; ii++) {
-      if (ii == 0) {
-        htmlString += data[i].Module.Assignment[ii];
-      } else {
-        htmlString += " and " + data[i].Module.Assignment[ii];
-      }
-    }
-    htmlString += " and Learning Outcome ";
-    for (ii = 0; ii < data[i].Module.Learning_outcomes.length; ii++) {
-      if (ii == 0) {
-        htmlString += data[i].Module.Learning_outcomes[ii];
-      } else {
-        htmlString += " and " + data[i].Module.Learning_outcomes[ii];
-      }
-    }
+  courses.forEach(course => {
+    console.log(course);
+    htmlString += "<p>" + course.Course + "</p>";
+  });
 
-    htmlString += " and Volume ";
-    for (ii = 0; ii < data[i].Module.Volume.length; ii++) {
-      if (ii == 0) {
-        htmlString += data[i].Module.Volume[ii];
-      } else {
-        htmlString += " and " + data[i].Module.Volume[ii];
-      }
-    }
-
-    htmlString += " and weights ";
-    for (ii = 0; ii < data[i].Module.weights.length; ii++) {
-      if (ii == 0) {
-        htmlString += data[i].Module.weights[ii];
-      } else {
-        htmlString += " and " + data[i].Module.weights[ii];
-      }
-    }
-    htmlString += ".</p>";
-  }
   moduleContainer.insertAdjacentHTML("beforeend", htmlString);
 }
